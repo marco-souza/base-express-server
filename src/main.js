33312a12b846bb1d6d4ffc170d98cdf7490f5900
Marco Antônio <ma.souza.junior@gmail.com>
2019-04-06 17:01:08 -0300

    ENH: improve comments

--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ import Server from './helpers/server'
 const main = () => {
     // Create app
     const app = express()
+
+    // Start server
     const options = {
       port: process.env.PORT || '3000',
       host: process.env.HOST || '0.0.0.0',
     }
     const server = new Server(app, options)
-
-    // Start server
     server.run()
 }
 
