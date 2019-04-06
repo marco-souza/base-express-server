c2f3dd9905851b5388c767ee7fb1db204f240357
Marco Antônio <ma.souza.junior@gmail.com>
2019-04-06 09:40:45 -0300

    FEA: create server with ping (health-check) endpoint

---
+++ src/main.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const main = () => {
+    // Create app
+    const app = express()
+
+    // Create health-check route
+    app.get('/ping', (req, res) => {
+        res.json({ message: 'pong' })
+    })
+
+    // Start to listen
+    const port = process.env.PORT || '3000'
+    const host = process.env.HOST || '0.0.0.0'
+
+    app.listen(port, host, () => {
+        console.log(`Start to listening in http://${host}:${port}`)
+    })
+}
+
+// Run main function
+main()
