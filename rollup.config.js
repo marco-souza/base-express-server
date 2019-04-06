7dbebacc542d1d61bed483199cc14df8ca63fc9c
Marco Antônio <ma.souza.junior@gmail.com>
2019-04-06 20:55:06 -0300

    FIX: remove binary build
    Since nexe and pkg had some problems, I decide to make it simple and maybe fix that in the future.

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import babel from 'rollup-plugin-babel'
 import json from 'rollup-plugin-json'
 import run from 'rollup-plugin-run'
-
 import paths from './filepaths.json'
 
 const dev = process.env.NODE_ENV !== 'production'
@@ -11,9 +9,7 @@ const dev = process.env.NODE_ENV !== 'production'
 export default {
   input: paths.server.entry,
   output: {
-    file: dev
-      ? 'build/server.js'
-      : undefined,
+    file: 'build/server.js',
     format: 'cjs',
   },
   external: [
@@ -23,16 +19,7 @@ export default {
     'swagger-ui-express',
   ],
   plugins: [
-    babel({
-        presets: ["@babel/preset-env"],
-        exclude: "node_modules/**"
-    }),
-    resolve({
-      mainFields: [
-        'jsnext',
-        'main',
-      ]
-    }),
+    resolve(),
     commonjs(),
     json(),
     dev && run(),
