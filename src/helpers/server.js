abd22e0d9730c2c5c1217548e69114f26528b278
Marco Antônio <ma.souza.junior@gmail.com>
2019-05-12 14:47:23 -0300

    REF: change server

--- src/helpers/server.js
+++ src/helpers/server.js
@@ -1,26 +1,44 @@
-import Router from './router'
+import express from 'express'
+import passport from 'passport'
 import bodyParser from 'body-parser'
 
+import setupDatabase from 'models'
+import Router from 'helpers/router'
+
 export default class Server {
-  constructor (app, { port, host }) {
-    this._app = app
-    this._host = host
-    this._port = port
-    this._startCallback = () => {
-      console.log(`Server is listening in http://${this._host}:${this._port}`)
-    }
-    // setup middlewares
+  constructor () {
+    // Create app
+    this._app = express()
+
+    // Start server
+    this._port = process.env.PORT || 3000
+    this._host = process.env.HOST || '0.0.0.0'
+
+    this._configure()
+  }
+
+  _configure = () => {
+    // setup middleware
     this._app.use(bodyParser.urlencoded({
       extended: true
     }))
     this._app.use(bodyParser.json())
+    this._app.use(passport.initialize())
+    this._app.use(passport.session())
 
     // define routers
-    this._router = new Router(app)
+    this._router = new Router(this._app)
     this._router.setupRoutes()
+
+    // Setup models
+    setupDatabase()
   }
 
-  run () {
+  _startCallback = () => {
+    console.log(`Server is listening in http://${this._host}:${this._port}`)
+  }
+
+  run = () => {
     // start server
     this._app.listen(
       this._port,
--- src/main.js
+++ src/main.js
@@ -1,18 +1,7 @@
-import express from 'express'
 import Server from 'helpers/server'
-import setupDatabase from 'models'
 
 const main = () => {
-  // Create app
-  const app = express()
-
-  // Start server
-  const options = {
-    port: process.env.PORT || '3000',
-    host: process.env.HOST || '0.0.0.0'
-  }
-  const server = new Server(app, options)
-  setupDatabase()
+  const server = new Server()
   server.run()
 }
 
